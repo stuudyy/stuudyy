91c9980bea5e8d557e30dcb331023589a319a3dd
Jakob Studeny <Jakob.Studeny@s.hakmistelbach.ac.at>
2021-02-19 09:26:07 +0100

    Klasse Projekte

---
+++ Zufallsgenerator/Zufallsgenerator/Projekte.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zufallsgenerator
+{
+    public class Projekte
+    {
+        private int nummer;
+        private string projekt;
+
+        private static int autonum = 1;
+
+        public Projekte() { }
+
+        public Projekte(string projekt)
+        {
+            nummer = autonum;
+            autonum++;
+
+            this.projekt = projekt;
+            
+        }
+
+        public int Nummer
+        {
+            get { return nummer; }
+            set { nummer = value; }
+        }
+
+        public string Projekt
+        {
+            get { return projekt; }
+            set { projekt = value; }
+        }
+
+        public static int Autonum
+        {
+            get { return autonum; }
+            set { autonum = value; }
+        }
+
+
+    }
+}
