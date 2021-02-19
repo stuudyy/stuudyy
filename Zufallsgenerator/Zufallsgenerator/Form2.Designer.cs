d65555258bc6f256491bc7f5e1d77e87c0f6997b
Florian07 <florian.haberle@s.hakmistelbach.ac.at>
2021-02-19 09:21:34 +0100

    Form2

---
+++ Zufallsgenerator/Zufallsgenerator/Form2.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Zufallsgenerator
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbbrechen = new System.Windows.Forms.Button();
+            this.btnSpeichern = new System.Windows.Forms.Button();
+            this.txtEingabe = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAbbrechen
+            // 
+            this.btnAbbrechen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbbrechen.Location = new System.Drawing.Point(220, 133);
+            this.btnAbbrechen.Name = "btnAbbrechen";
+            this.btnAbbrechen.Size = new System.Drawing.Size(215, 43);
+            this.btnAbbrechen.TabIndex = 0;
+            this.btnAbbrechen.Text = "Abbrechen";
+            this.btnAbbrechen.UseVisualStyleBackColor = true;
+            this.btnAbbrechen.Click += new System.EventHandler(this.btnAbbrechen_Click);
+            // 
+            // btnSpeichern
+            // 
+            this.btnSpeichern.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpeichern.Location = new System.Drawing.Point(474, 133);
+            this.btnSpeichern.Name = "btnSpeichern";
+            this.btnSpeichern.Size = new System.Drawing.Size(215, 43);
+            this.btnSpeichern.TabIndex = 1;
+            this.btnSpeichern.Text = "Speichern";
+            this.btnSpeichern.UseVisualStyleBackColor = true;
+            // 
+            // txtEingabe
+            // 
+            this.txtEingabe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEingabe.Location = new System.Drawing.Point(82, 80);
+            this.txtEingabe.Name = "txtEingabe";
+            this.txtEingabe.Size = new System.Drawing.Size(744, 27);
+            this.txtEingabe.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 214);
+            this.Controls.Add(this.txtEingabe);
+            this.Controls.Add(this.btnSpeichern);
+            this.Controls.Add(this.btnAbbrechen);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbbrechen;
+        private System.Windows.Forms.Button btnSpeichern;
+        private System.Windows.Forms.TextBox txtEingabe;
+    }
+}
